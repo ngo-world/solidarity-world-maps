cb7a829f748721110d85e19afd2b63e6b1185d50
David Gengenbach <info@davidgengenbach.de>
2024-12-14 23:14:57 +0100

    WIP

--- helpers/delete_map.py
+++ helpers/delete_map.py
@@ -14,6 +14,8 @@ def get_session(map_storage_api_key: str) -> requests.Session:
 
 # https://docs.workadventu.re/developer/map-storage-api/#deleting-a-wam-file
 def delete_map(*, session: requests.Session, map_storage_url: str, map_name: str):
+    if not map_name.startswith('/'):
+        map_name = '/' + map_name
     response = session.delete(f'{map_storage_url}{map_name}')
     response.raise_for_status()
 
@@ -29,12 +31,11 @@ map_storage_api_key = env['MAP_STORAGE_API_KEY']
 
 session = get_session(map_storage_api_key=map_storage_api_key)
 for map in get_maps(session=session, map_storage_url=map_storage_url):
-    print(map)
+    if input(f'Delete map "{map}"? Enter Y for yes: ').strip().lower().startswith('y'):
+        delete_map(
+            session=session,
+            map_storage_url=map_storage_url,
+            map_name=map
+        )
 
 
-for map_name in ['/dist/conference.wam', 'dist/office.wam']:
-    delete_map(
-        map_storage_url=map_storage_url,
-        map_storage_api_key=map_storage_api_key,
-        map_name=map_name
-    )
