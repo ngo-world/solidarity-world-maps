ba6cb3ef3114b426275656a28a69f762b928e1a4
David Gengenbach <info@davidgengenbach.de>
2024-12-14 17:46:14 +0100

    Cleanup

--- src/prototype_fund.ts
+++ src/prototype_fund.ts
@@ -1,6 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+import { UIWebsite } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+var smartphone: UIWebsite | null = null;
 
 console.log('Script started successfully');
 
@@ -15,35 +18,54 @@ WA.onInit().then(() => {
             scope: "world"
         });
     }
-    
+
 
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ', WA.player.tags)
     bootstrapExtra().then(async () => {
-        await WA.ui.website.open({
-            url: `https://127.0.0.1:4200?userId=${WA.player.playerId}`,
-            allowApi: true,
-            // ToDo
-            allowPolicy: 'microphone',
-            position: {
-                vertical: "bottom",
-                horizontal: "right",
-            },
-            size: {
-                height: "530px",
-                width: "235px",
-            },
+        WA.ui.actionBar.addButton({
+            id: 'toggleSmartPhoneButton',
+            label: 'Toggle smartphone',
+            callback: (_) => {
+                toggleSmartphone();
+            }
         });
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
 
+async function toggleSmartphone() {
+    if (!smartphone) {
+        await openSmartphone();
+    } else {
+        await (smartphone as unknown as UIWebsite).close();
+        smartphone = null;
+    }
+}
+
+async function openSmartphone() {
+    smartphone = await WA.ui.website.open({
+        url: `https://127.0.0.1:4200?userId=${WA.player.playerId}`,
+        allowApi: true,
+        // ToDo
+        allowPolicy: 'microphone',
+        position: {
+            vertical: "bottom",
+            horizontal: "right",
+        },
+        size: {
+            height: "530px",
+            width: "235px",
+        },
+    });
+}
+
 function getRandomPhoneNumber() {
     const min = 100000000;
     const max = 999999999;
-    const randomNumber = Math.floor(Math. random() * (max - min) + min);
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
     return `+${randomNumber}`;
 }
 
-export {};
+export { };
