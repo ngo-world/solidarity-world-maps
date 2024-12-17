bb35b67f67003258e6ecad18676f45b84bd4de43
Joewodsen <37843200+Joewodsen@users.noreply.github.com>
2024-12-17 13:09:01 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,15 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import {ActionMessage} from "@workadventure/iframe-api-typings";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-  let actionMessage: ActionMessage | undefined;
-
-  // When someone enters the bellZone area
-  WA.room.area.onEnter("bellZone").subscribe(() => {
-    // Display the action message
-    actionMessage = WA.ui.displayActionMessage({
-      type: "message",
-      message: "Press SPACE to ring the bell",
-      callback: () => {
-        // When space is pressed, we send a "bell-rang" signal to everyone on the map.
-        WA.event.broadcast("bell-rang", {});
-      }
-    });
-  });
-
-  // When someone leaves the bellZone area
-  WA.room.area.onLeave("bellZone").subscribe(() => {
-    if (actionMessage !== undefined) {
-      // Hide the action message
-      actionMessage.remove();
-      actionMessage = undefined;
-    }
-  });
-
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+    bootstrapExtra().then(async () => {
+    }).catch(e => console.error(e));
 }).catch(e => console.error(e));
 
 export {};
