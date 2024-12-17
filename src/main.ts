fd508451a26f52a4d846dafe48086d28c2080315
Joewodsen <37843200+Joewodsen@users.noreply.github.com>
2024-12-17 13:02:19 +0100

    ukrainbell

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,28 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 }).catch(e => console.error(e));
 
+  // When someone enters the bellZone area
+  WA.room.area.onEnter("bellZone").subscribe(() => {
+    // Display the action message
+    actionMessage = WA.ui.displayActionMessage({
+      type: "message",
+      message: "Press SPACE to ring the bell",
+      callback: () => {
+        // When space is pressed, we send a "bell-rang" signal to everyone on the map.
+        WA.event.broadcast("bell-rang", {});
+      }
+    });
+  });
+
+  // When someone leaves the bellZone area
+  WA.room.area.onLeave("bellZone").subscribe(() => {
+    if (actionMessage !== undefined) {
+      // Hide the action message
+      actionMessage.remove();
+      actionMessage = undefined;
+    }
+  });
+
+}).catch(e => console.error(e));
+
 export {};
